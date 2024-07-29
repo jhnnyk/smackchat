83fd81e1cd4dcc20c1635693004230ebb3760edb
John Knotts <jhnnyk@gmail.com>
2024-07-29 13:47:02 -0600

    user page design

--- src/pages/PageUsers.vue
+++ src/pages/PageUsers.vue
@@ -1,7 +1,54 @@
 <template>
-  <q-page class="flex">
-    <h5>Users page</h5>
+  <q-page class="flex q-pa-md">
+    <q-list class="full-width" separator>
+      <q-item
+        v-for="user in users"
+        :key="user.id"
+        to="/chat"
+        clickable
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name.charAt(0) }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge :color="user.online ? 'light-green-5' : 'grey-4'">{{
+            user.online ? 'Online' : 'Offline'
+          }}</q-badge>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+const users = [
+  {
+    id: 1,
+    name: 'Ruddy',
+    online: true,
+  },
+  {
+    id: 2,
+    name: 'Mallorie',
+    online: false,
+  },
+  {
+    id: 3,
+    name: 'Elizabeth',
+    online: true,
+  },
+  {
+    id: 4,
+    name: 'Jim',
+    online: true,
+  },
+]
+</script>
