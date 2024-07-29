d9810f203554120c7e9e2244bcdde23c02968d92
John Knotts <jhnnyk@gmail.com>
2024-07-29 13:58:02 -0600

    go back to user page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,6 +2,15 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
+        <q-btn
+          v-if="$route.fullPath.includes('/chat')"
+          icon="arrow_back"
+          flat
+          dense
+          label="Back"
+          @click="$router.go(-1)"
+        />
+
         <q-toolbar-title class="absolute-center">
           {{ title }}
         </q-toolbar-title>
@@ -15,21 +24,21 @@
 </template>
 
 <script setup>
-import { computed } from "vue";
-import { useRoute } from "vue-router";
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
-const route = useRoute();
+const route = useRoute()
 
 const title = computed(() => {
-  let currentPath = route.fullPath;
-  let titleText = "";
-  if (currentPath == "/") {
-    titleText = "SmackChat";
-  } else if (currentPath == "/chat") {
-    titleText = "Chat";
-  } else if (currentPath == "/auth") {
-    titleText = "Login";
+  let currentPath = route.fullPath
+  let titleText = ''
+  if (currentPath == '/') {
+    titleText = 'SmackChat'
+  } else if (currentPath == '/chat') {
+    titleText = 'Chat'
+  } else if (currentPath == '/auth') {
+    titleText = 'Login'
   }
-  return titleText;
-});
+  return titleText
+})
 </script>
